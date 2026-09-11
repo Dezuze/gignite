b0e0d26f2a49619dbb58b99238db886ae59ca9b3
Dezuze <weslyphilipj@gmail.com>
2026-09-11 08:41:32 +0530

    sanity

---
+++ studio/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'fzj1ump9',
+    dataset: 'production'
+  }
+})
---
+++ studio/schemas/registration.js
@@ -0,0 +1,97 @@
+export default {
+  name: 'registration',
+  title: 'Registration',
+  type: 'document',
+  fields: [
+    {
+      name: 'referenceCode',
+      title: 'Reference Code',
+      type: 'string',
+    },
+    {
+      name: 'teamName',
+      title: 'Team Name',
+      type: 'string',
+    },
+    {
+      name: 'college',
+      title: 'College / Institution',
+      type: 'string',
+    },
+    {
+      name: 'teamSize',
+      title: 'Team Size',
+      type: 'number',
+    },
+    {
+      name: 'leadName',
+      title: 'Leader Full Name',
+      type: 'string',
+    },
+    {
+      name: 'leadEmail',
+      title: 'Leader Email',
+      type: 'string',
+    },
+    {
+      name: 'leadPhone',
+      title: 'Phone Number',
+      type: 'string',
+    },
+    {
+      name: 'member2Name',
+      title: 'Member 2 Name',
+      type: 'string',
+    },
+    {
+      name: 'member2Email',
+      title: 'Member 2 Email',
+      type: 'string',
+    },
+    {
+      name: 'member3Name',
+      title: 'Member 3 Name',
+      type: 'string',
+    },
+    {
+      name: 'member3Email',
+      title: 'Member 3 Email',
+      type: 'string',
+    },
+    {
+      name: 'member4Name',
+      title: 'Member 4 Name',
+      type: 'string',
+    },
+    {
+      name: 'member4Email',
+      title: 'Member 4 Email',
+      type: 'string',
+    },
+    {
+      name: 'themeId',
+      title: 'Theme ID',
+      type: 'string',
+    },
+    {
+      name: 'problemStatement',
+      title: 'Problem Statement Summary',
+      type: 'text',
+    },
+    {
+      name: 'proposedSolution',
+      title: 'Proposed AI / Signal Processing Solution',
+      type: 'text',
+    },
+    {
+      name: 'techApproach',
+      title: 'Technical Stack & Tools',
+      type: 'string',
+    },
+    {
+      name: 'videoLink',
+      title: 'Pitch / Proof-of-Concept Video Link',
+      type: 'url',
+    },
+  ],
+};
